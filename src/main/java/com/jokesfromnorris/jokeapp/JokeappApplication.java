bbf66182364c646e619adbef49d09cc3bbc70e5c
enesoral <oralenes0@gmail.com>
2020-01-30 17:21:18 +0300

    Initialize add of project

---
+++ src/main/java/com/jokesfromnorris/jokeapp/JokeappApplication.java
@@ -0,0 +1,13 @@
+package com.jokesfromnorris.jokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokeappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jokesfromnorris/jokeapp/JokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jokesfromnorris.jokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
